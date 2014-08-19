dc3dde5cd2514daf6afc96ff9c1aeff19221d9b7
dpomaresp <dpomaresp@gmail.com>
2014-08-19 10:44:09 +0200

    Facebook y twitter icons

--- application/views/contact.php
+++ application/views/contact.php
@@ -62,6 +62,10 @@
 <!--==============================footer=================================-->
     <footer>
         <hr>
+        <div>
+            <a href="http://www.facebook.com/profile.php?id=100006764388620" target="_blank"><img class="social" src="/images/facebook_icon.png" alt="Facebook" /></a>
+            <a href="http://www.twitter.com/lavilamontiboli" target="_blank"><img class="social" src="/images/twitter_icon.png" alt="Twitter" /></a>
+        </div>
         <div class="fright">
         	<p>© 2014 Asociación Vecinos de Montíboli<br>
          		<a rel="nofollow" href="http://www.templatemonster.com/" target="_blank" class="link">
